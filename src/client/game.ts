5c6742be1ca858e205b33abe94bb740de75b19ef
kohei.hujita <point.out92@gmail.com>
2020-08-31 03:29:09 +0900

    mv game.ts

---
+++ src/client/game.ts
@@ -0,0 +1,11 @@
+import { assets } from "../engine/foundation/assets/assetLoader";
+import { DanmakuGame } from "./config/danmakuGame";
+import { resources } from "./config/resources";
+
+assets.addResources(resources);
+
+assets.loadAll().then(() => {
+  const game = new DanmakuGame();
+  game.addCanvas(game.canvas);
+  game.start();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/game.ts',
+  entry: './src/client/game.ts',
   watchOptions: {
     ignored: ["node_modules"],
   },
