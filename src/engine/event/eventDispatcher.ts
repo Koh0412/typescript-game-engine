9dc3fb99ae8b5d95aa8a01819dc78eabdf796d27
kohei.hujita <point.out92@gmail.com>
2020-09-05 22:30:53 +0900

    fix comment place

--- src/engine/event/eventDispatcher.ts
+++ src/engine/event/eventDispatcher.ts
@@ -2,9 +2,19 @@ import { GameEvent } from "./gameEvent";
 import { IEventListeners, EventKeyMap } from "../common/interfaces/event";
 
 export interface EventDispatcher {
+  /**
+   * `type`のイベントリスナーに`callback`を追加
+   * @param type
+   * @param callback
+   */
   addEventListener<K extends keyof EventKeyMap>(type: K, callback: (e: EventKeyMap[K]) => void): void;
   addEventListener(type: string, callback: (e: GameEvent) => void): void;
 
+  /**
+   * イベントの発信を行う
+   * @param type
+   * @param event
+   */
   dispatch<K extends keyof EventKeyMap>(type: K,  event: EventKeyMap[K]): void;
   dispatch(type: string,  event: GameEvent): void;
 }
@@ -16,11 +26,6 @@ export class EventDispatcher {
     this.eventListeners = {};
   }
 
-  /**
-   * `type`のイベントリスナーに`callback`を追加
-   * @param type
-   * @param callback
-   */
   addEventListener<K extends keyof EventKeyMap>(type: K, callback: (e: EventKeyMap[K]) => void): void {
     if (this.eventListeners[type] === undefined) {
       this.eventListeners[type] = [];
@@ -29,11 +34,6 @@ export class EventDispatcher {
     this.eventListeners[type].push(callback);
   }
 
-  /**
-   * イベントの発信を行う
-   * @param type
-   * @param event
-   */
   dispatch<K extends keyof EventKeyMap>(type: K,  event: EventKeyMap[K]): void {
     const listeners = this.eventListeners[type];
     if (listeners !== undefined) {
